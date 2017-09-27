e1af373fde55a0c9287800193c204b787d2adf99
Elena G <elena.guzbla@gmail.com>
2017-09-27 14:43:46 +0200

    wip intent a viewMsg

--- app/src/main/java/com/example/sendmessage/SendMessage.java
+++ app/src/main/java/com/example/sendmessage/SendMessage.java
@@ -1,13 +1,49 @@
 package com.example.sendmessage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SendMessage extends AppCompatActivity {
+
+    // Instancias a utilizar
+    private EditText edtT_Message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
+
+        // Obteniendo edtText para mensaje
+        edtT_Message = (EditText) findViewById(R.id.edtT_Message);
     }
+
+    public void manageOnClick(View v)
+    {
+        // ¿Qué botón hemos pulsado?
+        switch (v.getId()) {
+            case R.id.btn_SendMsg:
+                enviarMsg();
+                break;
+        }
+    }
+
+    void enviarMsg()
+    {
+        // Recoger usuario y mensaje
+        // Crear Bundle y añadir el mensaje
+        Bundle bndl = new Bundle();
+        bndl.putString("msg", edtT_Message.getText().toString());
+
+        // Crear y cargar Intent con Bundle
+        Intent intnt = new Intent(SendMessage.this, ViewMsg.class);
+        intnt.putExtras(bndl);
+
+        // Iniciar la Activity ViewMessage
+        startActivity(intnt);
+
+    }
+
 }
